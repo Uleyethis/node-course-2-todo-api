875b47a28c38d2863d600a873c58eb479c69cb4c
Uley <street.kid.23@gmail.com>
2018-12-13 07:19:23 -0500

    creating schemas and adding new collections to database

--- server/server.js
+++ server/server.js
@@ -6,33 +6,48 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 const Todo = mongoose.model('Todo', {
   text: {
     type: String,
+    required: true,
+    minlength: 1,
+    trim: true,
   },
   completed: {
     type: Boolean,
+    default: false,
   },
   completedAt: {
     type: Number,
+    default: null,
   },
 });
 
 // const newTodo = new Todo({
-//   text: 'Cook dinner',
+//   text: '  Edit this video     ',
 // });
 
 // newTodo.save().then((doc) => {
-//   console.log('Saved Todo', doc);
+//   console.log(JSON.stringify(doc, undefined, 2));
 // }, (e) => {
-//   console.log('Unable to save Todo.');
+//   console.log('Unable to save Todo.', e);
 // });
 
-const newTodo = new Todo({
-  text: 'Eat dinner',
-  completed: false,
-  completedAt: 0,
+// User Model
+// email - required, trimmed, type String, min length 1
+
+const User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true,
+  },
 });
 
-newTodo.save().then((doc) => {
-  console.log('Saved Todo', doc);
+const newUser = new User({
+  email: '  some@email.com     ',
+});
+
+newUser.save().then((doc) => {
+  console.log(JSON.stringify(doc, undefined, 2));
 }, (e) => {
-  console.log('Unable to save Todo.');
+  console.log('Unable to save User.', e);
 });
